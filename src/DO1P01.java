093a629019a5c55c6ac68e0cf5c97f642bc7a29e
Abhishek Yadav <monuy8830@gmail.com>
2024-03-02 00:32:55 +0530

    setup

---
+++ src/DO1P01.java
@@ -0,0 +1,29 @@
+public class DO1P01 {
+    public class book {
+        private string book_title;
+        private float book_price;
+
+        public string getBookTitle() {
+            return book_title;
+        }
+
+        public float getBook_price() {
+            return book_price;
+        }
+        public string setBookTitle( string book_title) {
+            return book_title;
+        }
+
+        public float setBook_price(float book_price) {
+            return book_price;
+        }
+
+        public class Main {
+            public static void main(String[] args) {
+
+            }
+        }
+
+    }
+
+}
