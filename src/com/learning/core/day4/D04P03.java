c3aab1cc3f7837bf983521d25d33130d550d07e9
Abhishek Yadav <monuy8830@gmail.com>
2024-03-06 23:56:43 +0530

    day 4

---
+++ src/com/learning/core/day4/D04P03.java
@@ -0,0 +1,13 @@
+package com.learning.core.day4;
+
+public class D04P03 {
+    String applicantName;
+    String postApplied;
+    int applicantAge;
+    public D04P03(String applicantName, String postApplied, int applicantAge){
+        this.applicantName = applicantName;
+        this.postApplied = postApplied;
+        this.applicantAge = applicantAge;
+
+    }
+}
---
+++ src/com/learning/core/day4/Validator.java
@@ -0,0 +1,4 @@
+package com.learning.core.day4;
+
+public class Validator {
+}
